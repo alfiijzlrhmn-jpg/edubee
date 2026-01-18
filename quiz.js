a1f2049a375dba131679936b1b95fd50f911a209
alfiijzlrhmn-jpg <alfiijazulrahman@gmail.com>
2026-01-19 02:43:51 +0700

    Delete quiz.js

--- quiz.js
+++
@@ -1,9 +0,0 @@
-function cekJawaban() {
-  let jawaban = prompt("Masukkan jawaban:");
-
-  if (jawaban.toLowerCase() === "continuously variable transmission") {
-    document.getElementById("hasil").innerText = "Benar ✅";
-  } else {
-    document.getElementById("hasil").innerText = "Salah ❌";
-  }
-}
